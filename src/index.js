03d17f24420416b92430009f8563af2d190cca1e
shavla <57368636+shavla@users.noreply.github.com>
2020-06-08 14:22:48 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,26 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import axios from 'axios';
 
-// class App extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             apiResponse: ""
-//         }
-//     }
-//     callAPI() {
-//         fetch("http://localhost:4000/testAPI")
-//             .then(res => res.text())
-//             .then(res => this.setState({ apiResponse: res }));
-//     }
-//     componentWillMount() {
-//         this.callAPI();
-//     }
-//     render() {
-//         return (
-//             <p>{this.state.apiResponse}</p>
-//         )
-//     }
-// }
 ReactDOM.render(<App />, document.getElementById('root'));
-
